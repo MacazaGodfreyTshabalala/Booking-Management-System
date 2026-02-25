cf2fab807a1428551945450cfcfcce99b1fcc06c
Macaza Tshabalala <macazatshabalala726@gmail.com>
2026-02-25 15:17:42 +0200

    Create Project

---
+++ Bookings Management System/src/bookings/management/system/BookingsManagementSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bookings.management.system;
+
+/**
+ *
+ * @author Tshabalala.M
+ */
+public class BookingsManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
